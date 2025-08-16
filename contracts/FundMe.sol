2147a171c97fd65e31dd159b2cad4072cd736ae6
2022148071 <kangwon71@yonsei.ac.kr>
2025-08-16 16:52:53 +0900

    ~ 0816. thresholds

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.6 <0.9.0;
+pragma solidity ^0.8.13;
 
 
 import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 // able to accept some type of payment
 contract FundMe {
 
@@ -11,6 +12,10 @@ contract FundMe {
 
     //func that can accept payment
     function fund() public payable{
+        //$5
+        uint256 minimumUSD = 5*(10**18);
+        // 1gwei < $5
+        require(getConversionRate(msg.value) >= minimumUSD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         // what the ETH -> USD coinversion rate
     }
@@ -22,15 +27,16 @@ contract FundMe {
 
     function getPrice() public view returns (uint256){
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (
-            uint80 roundId, 
-            int256 answer, 
-            uint256 startedAt, 
-            uint256 updatedAt, 
-            uint80 answeredInRound
-        ) = priceFeed.latestRoundData();
+        (,int256 answer,,,) = priceFeed.latestRoundData(); //변수 5개인데 4개는 안 써서
         return uint256(answer);
         //4,663.03720859 * 10**8
     }
-
+    //1000000000
+    //converts value that they send to its USD equivalent
+    function getConversionRate(uint256 ethAmount) public view returns (uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice*ethAmount) / (10**8);
+        return ethAmountInUsd;
+        // 0.000004433115724000
+    }
 }
